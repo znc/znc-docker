b6c195ffb583e7c86f3df859a38d3a244b76eb02
Pierce McGoran <znc@piercemcgoran.com>
2018-09-06 12:05:22 -0400

    Avoid needlessly running znc-buildmod at every startup

--- full/30-build-modules.sh
+++ full/30-build-modules.sh
@@ -3,15 +3,23 @@
 if [ -d "${DATADIR}/modules" ]; then
     cd "${DATADIR}/modules" || exit 11
 
-    # Find module sources.
-    modules=$(find . -name "*.cpp")
-
-    if [ -n "$modules" ]; then
-        # Build modules.
-        echo "Building modules $modules..."
-        /opt/znc/bin/znc-buildmod $modules || exit 12
+    # if version file doesn't exist, create it
+    if [ ! -f ".version" ]; then
+        touch .version
     fi
 
+    # Check version we were at when znc-buildmod was last run
+    if [ "$ZNC_VERSION" != "$(cat .version)" ]; then
+
+        # Find module sources.
+        modules=$(find . -name "*.cpp")
+
+        if [ -n "$modules" ]; then
+            # Build modules.
+            echo "Building modules $modules..."
+            /opt/znc/bin/znc-buildmod $modules || exit 12
+        fi
+        echo -n $ZNC_VERSION > .version
+    fi
     cd /
 fi
-
